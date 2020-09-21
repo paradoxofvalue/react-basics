3d970f9fedebe4ecb584458d8bb623a6b283d4fb
Vadym Lavorchuk <lavorchuk.vadim@digitalsuits.co>
2020-09-21 23:08:40 +0300

    monsters in a state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,18 +7,27 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello',
+      monsters: [
+        {
+          id: 123,
+          name: 'Drakula'
+        },
+        {
+          id: 321,
+          name: 'Zombie'
+        }
+      ],
     }
   }
 
   render() {
+    const { monsters } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({string: 'test'})}>Change text</button>
-        </header>
+        {
+          monsters.map((monster, index) => <h1 key={ index }>{ monster.name } + { monster.id }</h1>)
+        }
       </div>
     )
   }
