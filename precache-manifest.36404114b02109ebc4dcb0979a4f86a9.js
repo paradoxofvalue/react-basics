ace691acb33ba95087ee4991aa722b54e82fa317
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2020-09-29 22:45:37 +0300

    Updates

---
+++ precache-manifest.36404114b02109ebc4dcb0979a4f86a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894022fa49f5168127e51f62190664e4",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "7e21358b525c46441bf1",
+    "url": "/react-basics/static/css/main.1cf68c2b.chunk.css"
+  },
+  {
+    "revision": "2aa89dde0859bdbbf2dc",
+    "url": "/react-basics/static/js/2.c3b51709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics/static/js/2.c3b51709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e21358b525c46441bf1",
+    "url": "/react-basics/static/js/main.0bb260b5.chunk.js"
+  },
+  {
+    "revision": "ecb067e5745a49c636d5",
+    "url": "/react-basics/static/js/runtime-main.74e35950.js"
+  }
+]);
